d63085bbd9bba01876d34ad67c3c6f1908312a89
arshian <arshian.ali.dev@gmail.com>
2026-06-21 12:52:02 +0500

    chore: remove scratch directory

--- scratch/test.js
+++
@@ -1,4 +0,0 @@
-import Prism from 'prismjs';
-import 'prismjs/components/prism-javascript.js';
-
-console.log(Object.keys(Prism.languages));
--- scratch/test2.js
+++
@@ -1,6 +0,0 @@
-import Prism from 'prismjs';
-import 'prismjs/components/prism-javascript.js';
-import 'prismjs/components/prism-markdown.js';
-
-const md = "```js\nconst x = 1;\n```";
-console.log(Prism.highlight(md, Prism.languages.markdown, 'markdown'));
